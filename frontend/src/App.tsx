b0b18d01d8ea0f4708db362dc32dc3279d61068d
Arslan Javaid <aja118379@gmail.com>
2024-03-31 02:05:20 +0500

    Practised chadcn with tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
+import { EnvelopeClosedIcon } from "@radix-ui/react-icons"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button>Click Me</Button>
+      <Button variant="ghost">Click Me</Button>
+      <Button variant="outline">Click Me</Button>
+      <Button variant="link">Click Me</Button>
+      <Button disabled>
+        <EnvelopeClosedIcon className="mr-2 h-4 w-4 animate-ping" />
+        Please wait
+      </Button>
+      <Button className='animate-pulse hover:bg-red-600'>
+        <EnvelopeClosedIcon className="mr-2 h-4 w-4" />
+        Please wait
+      </Button>
+      <h1 className='animate-bounce'>Hello</h1>
     </>
   )
 }
